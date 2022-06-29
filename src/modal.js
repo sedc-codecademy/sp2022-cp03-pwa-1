968f7977c81d1553b49f8065f39688a4d18984d3
Gregori <greg.gego@gmail.com>
2022-06-29 17:33:16 +0200

    updatedU

--- src/modal.js
+++ src/modal.js
@@ -2,17 +2,6 @@ const swiper1 = document.getElementById("swiper");
 const modal = document.getElementById("modal-sign-in");
 const modalSingUp = document.getElementById("modal-sign-up");
 const singUp = document.getElementById("sign-up")
-
-// privremeno kolku da funcionira za demoto
-const navBar = document.querySelector(".nav-container");
-const arrowPrev = document.querySelector(".swiper-button-prev");
-const arrowNext = document.querySelector(".swiper-button-next");
-navBar.style.display = "none";
-arrowPrev.style.display = "none";
-arrowNext.style.display = "none";
-
-
-
 const exits = document.querySelectorAll('#modal-exit');
 
 exits.forEach(function(exit) {
@@ -20,10 +9,8 @@ exits.forEach(function(exit) {
         event.preventDefault();
         modal.classList.remove('open');
         swiper1.classList.remove('z-index')
-            //  privremeno kolku da funcionira za demoto 
-        navBar.style.display = "flex";
-        arrowPrev.style.display = "flex";
-        arrowNext.style.display = "flex";
+
+
     });
 })
 
