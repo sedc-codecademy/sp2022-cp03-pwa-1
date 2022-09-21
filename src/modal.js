cb350c1d2ccb5b03554495ac0198d45c6eb8d567
Gregori <greg.gego@gmail.com>
2022-09-21 23:41:38 +0200

    update

--- src/modal.js
+++ src/modal.js
@@ -1,50 +1,49 @@
-const swiper1 = document.getElementById("swiper");
-const swiperNavArrows = document.querySelector(".swiperBarArrows");
-const navigationBar = document.querySelector(".nav-container");
-const modal = document.getElementById("modal-sign-in");
-const modalSingUp = document.getElementById("modal-sign-up");
-const singUp = document.getElementById("sign-up")
-const exits = document.querySelectorAll('#modal-exit');
+// const swiper1 = document.getElementById("swiper");
+// const swiperNavArrows = document.querySelector(".swiperBarArrows");
+// const navigationBar = document.querySelector(".nav-container");
+// const modal = document.getElementById("modal-sign-in");
+// const modalSingUp = document.getElementById("modal-sign-up");
+// const singUp = document.getElementById("sign-up")
+// const exits = document.querySelectorAll('#modal-exit');
 
-exits.forEach(function (exit) {
+// exits.forEach(function (exit) {
 
-    if (exit.innerHTML === 'Sign in as a guest') {
-        exit.addEventListener('click', function (event) {
-            event.preventDefault();
+//     if (exit.innerHTML === 'Sign in as a guest') {
+//         exit.addEventListener('click', function (event) {
+//             event.preventDefault();
 
-            localStorage.setItem("isGuest", "true")
+//             localStorage.setItem("isGuest", "true")
 
-            document.getElementById("arrows").style.display = "none"
+//             document.getElementById("arrows").style.display = "none"
 
-            modal.classList.remove('open');
-            swiper1.classList.remove('z-index');
-            swiperNavArrows.classList.remove('z-index');
-            navigationBar.classList.remove('z-index');
+//             modal.classList.remove('open');
+//             swiper1.classList.remove('z-index');
+//             swiperNavArrows.classList.remove('z-index');
+//             navigationBar.classList.remove('z-index');
 
-            setTimeout(function () {
-                for (let i = 0; i < 5; i++) {
+//             setTimeout(function () {
+//                 for (let i = 0; i < 5; i++) {
 
-                    const swiper = document.getElementById("swiper" + i)
+//                     const swiper = document.getElementById("swiper" + i)
 
-                    if (swiper.innerText.toLocaleLowerCase() !== 'home') {
-                        swiper.style.pointerEvents = "none"
-                    }
-                }
-            }, 100)
-        });
-    } else {
-        exit.addEventListener('click', function (event) {
-            event.preventDefault();
-            modal.classList.remove('open');
-            modalSingUp.classList.remove('open')
-            swiper1.classList.remove('z-index');
-            swiperNavArrows.classList.remove('z-index');
-            navigationBar.classList.remove('z-index');
-        });
-    }
-})
+//                     if (swiper.innerText.toLocaleLowerCase() !== 'home') {
+//                         swiper.style.pointerEvents = "none"
+//                     }
+//                 }
+//             }, 100)
+//         });
+//     } else {
+//         exit.addEventListener('click', function (event) {
+//             event.preventDefault();
+//             modal.classList.remove('open');
+//             modalSingUp.classList.remove('open')
+//             swiper1.classList.remove('z-index');
+            
+//         });
+//     }
+// })
 
-singUp.addEventListener('click', function (event) {
-    modalSingUp.classList.add('open')
-    modal.classList.remove('open')
-})
+// singUp.addEventListener('click', function (event) {
+//     modalSingUp.classList.add('open')
+//     modal.classList.remove('open')
+// })
--- src/script.js
+++ src/script.js
@@ -1,53 +1,84 @@
 // Swiper
 
-const menu = ['CALENDAR', 'STATISTICS', 'HOME', 'PRODUCTIVITY', 'REMINDERS']
+// const menu = ['CALENDAR', 'STATISTICS', 'HOME', 'PRODUCTIVITY', 'REMINDERS']
 const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
-    effect: 'coverflow',
+    keyboardControl: true,
     grabCursor: false,
     centeredSlides: true,
     slidesPerView: 'auto',
     initialSlide: 2,
     allowTouchMove: false,
-
-
-
-    coverflowEffect: {
-        rotate: 0,
-        stretch: 0,
-        depth: 1500,
-        modifier: 1.2,
-        slideShadows: true,
-    },
-
-    // Pagination a.k.a "the navigation-bar"
-    pagination: {
-        el: '.swiper-pagination',
-        transparent: false,
-        clickable: true,
-        renderBullet: function(index, className) {
-            return '<span class="' + className + '">' + (menu[index]) + '</span>';
-        },
-
-
-
-    },
-
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-
-
-    },
-
-
+    speed: 600,
 });
 
 
 
+// Scroll page on refresh
 
+window.onbeforeunload = () => {  
+    window.scrollTo(0, 0);  
+};
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        $(".icon-scroll").css("opacity", 1 - $(window).scrollTop() / $('.icon-scroll').height());
+    });
+  });
+
+(function($) { "use strict";
+// navigation
+$('a[data-slide]').click(function(e) {
+  e.preventDefault();
+  swiper.slideTo($(this).data('slide'));
+});
+})(jQuery);
+
+window.addEventListener('load',function(){
+    document.getElementsByTagName('header')[0].addEventListener('click',function(){
+      toggle_menu();
+    });
+  });
+
+window.addEventListener('scroll',function(){
+ header_scroll();
+    
+});
+  
+function header_scroll(){
+    var my_header = document.getElementsByTagName('header')[0];
+    var scroll_height = window.pageYOffset;
+    if(scroll_height > 650){
+      my_header.className = 'header header-scroll';
+      
+    }
+    else {
+      my_header.className = 'header';
+    }
+  }
+
+function toggle_menu(){
+    var menu_bar = document.querySelector('.header ul');
+    var btn = document.getElementsByClassName('toggle-btn')[0];
+    //btn.innerHTML = '&times;';
+    if(document.documentElement.clientWidth <= 768){
+    //if(menu_bar.style.display == 'none'){
+      if(menu_bar.className == 'display-none-mobile'){
+        menu_bar.className = 'display-block';
+        menu_bar.style.animation = 'flip 1 0.4s 0s';
+        btn.innerHTML = '&times;';
+        btn.style.fontSize = '40px';
+        btn.style.animation = 'fade 1 0.2s 0s';
+      }
+      else if(menu_bar.className == 'display-block'){
+       menu_bar.className = 'display-none-mobile';
+        btn.innerHTML = '&#9776';
+        btn.style.fontSize = '30px';
+        btn.style.animation = 'top-in-basic 1 0.2s 0s';
+      }
+   }
+  }
 
 
 
