4f1605a2dafc7ead2a74a3d5a1a39c1866981c18
Gregori <greg.gego@gmail.com>
2022-09-30 11:50:19 +0200

    login

--- src/logIn/logIn.js
+++ src/logIn/logIn.js
@@ -6,9 +6,6 @@ let html = document.querySelector("html");
 
 // Vars
 
-let swiperSlides = [];
-let header;
-
 // Func for creating HTML DOM Elements
 
 let createElements= (initObj)=> {
@@ -25,6 +22,11 @@ let createElements= (initObj)=> {
     return element;
 };
 
+let swiperSlides = [];
+let headerDiv = [];
+let messageDiv = [];
+let imageDiv = [];
+
 getStartedButton.addEventListener("click", function() {
     mainPage.classList.toggle("hidden");
     html.classList.toggle("htmlSet");
@@ -34,15 +36,65 @@ getStartedButton.addEventListener("click", function() {
     let swiperWrapper = createElements({Tag: "div", classList: "swiper-wrapper log-in-wrapper", childNodes: [swiperForLogIn]});
     const swiperPagination = createElements({Tag: "div", classList: "swiper-pagination",childNodes: [leftDiv]})
 
-    
-
     for(let i = 0; i < 4; i++) {
         swiperSlides.push(createElements({Tag: "div", classList: "swiper-slide slides-log-in", childNodes: [swiperWrapper]}));
     };
 
     for(let i = 0; i < swiperSlides.length; i++) {
-        swiperSlides[1].innerHTML = "I have changed!";
+        headerDiv.push(createElements({Tag: "div", classList: "logIn-header-div gap", childNodes: [swiperSlides[i]]}));
+        messageDiv.push(createElements({Tag: "div", classList: "logIn-message-div gap", childNodes: [swiperSlides[i]]}));
+        imageDiv.push(createElements({Tag: "div", classList: "logIn-image-div gap", childNodes: [swiperSlides[i]]}));
     };
+
+    // main log in tab
+
+    let rightDiv = createElements({Tag: "div", classList: "log-in-div", childNodes: [appLogIn]});
+    let logInTab = createElements({Tag: "div", classList: "log-in-tab", childNodes: [rightDiv]});
+    let logInMainTab = createElements({Tag: "div", classList: "log-in-main-tab", childNodes: [logInTab]});
+    let logInMainTabHeader = createElements({Tag: "div", classList: "log-in-main-tab-header", childNodes: [logInMainTab]});
+    let logInMainTabForm = createElements({Tag: "div", classList: "log-in-main-tab-form", childNodes: [logInMainTab]});
+
+    logInMainTabHeader.innerHTML = `<h2>Sign in<h2/>`
+    logInMainTabForm.innerHTML = `
+        <form id="login-form">
+            <p>
+            <input type="text" id="username" name="username" placeholder="Username" required><i class="validation"><span></span><span></span></i>
+            </p>
+            <p>
+            <input type="email" id="email" name="email" placeholder="Email Address" required><i class="validation"><span></span><span></span></i>
+            </p>
+            <p>
+            <input type="submit" id="login" value="Login">
+            </p>
+        </form>
+        <div id="create-account-wrap">
+            <p>Not a member? <a href="#">Create Account</a><p>
+        </div>`
+    
+
+    // swiper-slide [0]
+
+    headerDiv[0].innerHTML = `<h1>Mange your day</h1>`
+    messageDiv[0].innerHTML = `<p>From groceries to picking up the kids, we help you remember it all, anytime, anywhere.</p>`
+    imageDiv[0].innerHTML = `<img src="./media/logIn-media/4703428.jpg">`
+
+    // swiper-slide [1]
+
+    headerDiv[1].innerHTML = `<h1>Get anything done</h1>`
+    messageDiv[1].innerHTML = `<p>Create your perfect daily routine that works for you.</p>`
+    imageDiv[1].innerHTML = `<img src="./media/logIn-media/4905798.jpg">`
+
+    // swiper-slide [2]
+
+    headerDiv[2].innerHTML = `<h1>Never forget a thing</h1>`
+    messageDiv[2].innerHTML = `<p>Easily add reminders for daily, weekly or monthly commitments.</p>`
+    imageDiv[2].innerHTML = `<img src="./media/logIn-media/6334182.jpg">`
+
+    // swiper-slide [2]
+
+    headerDiv[3].innerHTML = `<h1>Live Strategically</h1>`
+    messageDiv[3].innerHTML = `<p>Focus on the things that matter to you unlock your true potential</p>`
+    imageDiv[3].innerHTML = `<img src="./media/logIn-media/5243332.jpg">`
     
    const logInSwiper = new Swiper('.swiper-log-in', {
         // Optional parameters
@@ -63,12 +115,7 @@ getStartedButton.addEventListener("click", function() {
             delay: 7000,
         },
         effect: 'fade',
-        coverflowEffect: {
-            rotate: 0,
-            slideShadows: false,
-            depth: 3500,
-            
-        },
+        
     });
 })
 
