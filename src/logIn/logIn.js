72a2290568ff9e5e4738b41d474c67f9ae3674da
DDarkoO <ddejanoski@gmail.com>
2022-10-02 21:39:01 +0200

    updateGreg

--- src/logIn/logIn.js
+++ src/logIn/logIn.js
@@ -9,7 +9,7 @@ let html = document.querySelector("html");
 
 // Func for creating HTML DOM Elements
 
-let createElements= (initObj)=> {
+let createElements = (initObj) => {
     var element = document.createElement(initObj.Tag);
     for (var prop in initObj) {
         if (prop === "childNodes") {
@@ -31,47 +31,47 @@ let imageDiv = [];
 
 
 getStartedButton.forEach((item) => {
-    item.addEventListener("click", function() {
+    item.addEventListener("click", function () {
 
         mainPage.classList.toggle("hidden");
         html.classList.toggle("htmlSet");
-        
+
         //left div
 
-        let appLogIn = createElements({Tag: "div", classList: "appLogIn", childNodes: [html]});
-        let leftDiv = createElements({Tag: "div", classList: "sliderShow", childNodes: [appLogIn]});
-        let swiperForLogIn = createElements({Tag: "div", classList: "swiper-log-in", childNodes: [leftDiv]});
-        let swiperWrapper = createElements({Tag: "div", classList: "swiper-wrapper log-in-wrapper", childNodes: [swiperForLogIn]});
-        const swiperPagination = createElements({Tag: "div", classList: "swiper-pagination",childNodes: [leftDiv]})
-        
-        for(let i = 0; i < 4; i++) {
-            swiperSlides.push(createElements({Tag: "div", classList: "swiper-slide slides-log-in", childNodes: [swiperWrapper]}));
+        let appLogIn = createElements({ Tag: "div", classList: "appLogIn", childNodes: [html] });
+        let leftDiv = createElements({ Tag: "div", classList: "sliderShow", childNodes: [appLogIn] });
+        let swiperForLogIn = createElements({ Tag: "div", classList: "swiper-log-in", childNodes: [leftDiv] });
+        let swiperWrapper = createElements({ Tag: "div", classList: "swiper-wrapper log-in-wrapper", childNodes: [swiperForLogIn] });
+        const swiperPagination = createElements({ Tag: "div", classList: "swiper-pagination", childNodes: [leftDiv] })
+
+        for (let i = 0; i < 4; i++) {
+            swiperSlides.push(createElements({ Tag: "div", classList: "swiper-slide slides-log-in", childNodes: [swiperWrapper] }));
         };
-    
-        for(let i = 0; i < swiperSlides.length; i++) {
-            headerDiv.push(createElements({Tag: "div", classList: "logIn-header-div gap", childNodes: [swiperSlides[i]]}));
-            messageDiv.push(createElements({Tag: "div", classList: "logIn-message-div gap", childNodes: [swiperSlides[i]]}));
-            imageDiv.push(createElements({Tag: "div", classList: "logIn-image-div gap", childNodes: [swiperSlides[i]]}));
+
+        for (let i = 0; i < swiperSlides.length; i++) {
+            headerDiv.push(createElements({ Tag: "div", classList: "logIn-header-div gap", childNodes: [swiperSlides[i]] }));
+            messageDiv.push(createElements({ Tag: "div", classList: "logIn-message-div gap", childNodes: [swiperSlides[i]] }));
+            imageDiv.push(createElements({ Tag: "div", classList: "logIn-image-div gap", childNodes: [swiperSlides[i]] }));
         };
-    
+
         // main log in & sign up tab right div
-        let rightDiv = createElements({Tag: "div", classList: "log-in-div", childNodes: [appLogIn]});
+        let rightDiv = createElements({ Tag: "div", classList: "log-in-div", childNodes: [appLogIn] });
 
-        let swiperRight = createElements({Tag: "div", classList: "right-swiper", childNodes: [rightDiv]});
-        let swiperWrapperRight = createElements({Tag: "div", classList: "swiper-wrapper wrapper-right", childNodes: [swiperRight]});
-        
-        
-        let swiperSlidesRightAtZero = createElements({Tag: "div", classList: "swiper-slide slides-other", childNodes: [swiperWrapperRight]});
-        let swiperSlidesRightAtOne = createElements({Tag: "div", classList: "swiper-slide slides-other", childNodes: [swiperWrapperRight]});
+        let swiperRight = createElements({ Tag: "div", classList: "right-swiper", childNodes: [rightDiv] });
+        let swiperWrapperRight = createElements({ Tag: "div", classList: "swiper-wrapper wrapper-right", childNodes: [swiperRight] });
 
-        
-    
-        
-        let logInTab = createElements({Tag: "div", classList: "log-in-tab", childNodes: [swiperSlidesRightAtZero]});
-        let logInMainTab = createElements({Tag: "div", classList: "log-in-main-tab", childNodes: [logInTab]});
-        let logInMainTabHeader = createElements({Tag: "div", classList: "log-in-main-tab-header", childNodes: [logInMainTab]});
-        let logInMainTabForm = createElements({Tag: "div", classList: "log-in-main-tab-form", childNodes: [logInMainTab]});
-    
+
+        let swiperSlidesRightAtZero = createElements({ Tag: "div", classList: "swiper-slide slides-other", childNodes: [swiperWrapperRight] });
+        let swiperSlidesRightAtOne = createElements({ Tag: "div", classList: "swiper-slide slides-other", childNodes: [swiperWrapperRight] });
+
+
+
+
+        let logInTab = createElements({ Tag: "div", classList: "log-in-tab", childNodes: [swiperSlidesRightAtZero] });
+        let logInMainTab = createElements({ Tag: "div", classList: "log-in-main-tab", childNodes: [logInTab] });
+        let logInMainTabHeader = createElements({ Tag: "div", classList: "log-in-main-tab-header", childNodes: [logInMainTab] });
+        let logInMainTabForm = createElements({ Tag: "div", classList: "log-in-main-tab-form", childNodes: [logInMainTab] });
+
         logInMainTabHeader.innerHTML = `<h2>Sign in<h2/>`
         logInMainTabForm.innerHTML = `
             <form id="login-form">
@@ -88,16 +88,16 @@ getStartedButton.forEach((item) => {
             <div id="create-account-wrap">
                 <p>Not a member? <a href="#" data-slide="2">Create Account</a><p>
             </div>`
-        
+
         // sign up tab
 
-        let rightDivSingUp = createElements({Tag: "div", classList: "sign-up-div", childNodes: [swiperSlidesRightAtOne]});
-        let singUpTab = createElements({Tag: "div", classList: "sign-up-tab", childNodes: [rightDivSingUp]});
-        let signUpMainTab = createElements({Tag: "div", classList: "sign-up-main-tab", childNodes: [singUpTab]});
-        let backToLogIn = createElements({Tag: "div", classList: "back-to-log", childNodes: [signUpMainTab]});
-        let signUpMainTabHeader = createElements({Tag: "div", classList: "sign-up-main-tab-header", childNodes: [signUpMainTab]});
-        let signUpMainTabForm = createElements({Tag: "div", classList: "sign-up-main-tab-form", childNodes: [signUpMainTab]});
-        backToLogIn.innerHTML = `<a href="#" data-slide="1"><img src="./media/logIn-media/icons8-left-50.png"></a>`
+        let rightDivSingUp = createElements({ Tag: "div", classList: "sign-up-div", childNodes: [swiperSlidesRightAtOne] });
+        let singUpTab = createElements({ Tag: "div", classList: "sign-up-tab", childNodes: [rightDivSingUp] });
+        let signUpMainTab = createElements({ Tag: "div", classList: "sign-up-main-tab", childNodes: [singUpTab] });
+        let backToLogIn = createElements({ Tag: "div", classList: "back-to-log", childNodes: [signUpMainTab] });
+        let signUpMainTabHeader = createElements({ Tag: "div", classList: "sign-up-main-tab-header", childNodes: [signUpMainTab] });
+        let signUpMainTabForm = createElements({ Tag: "div", classList: "sign-up-main-tab-form", childNodes: [signUpMainTab] });
+        backToLogIn.innerHTML = `<a href="#" data-slide="0"><img src="./media/logIn-media/icons8-left-50.png"></a>`
         signUpMainTabHeader.innerHTML = `<h2>Sign up<h2/>`
         signUpMainTabForm.innerHTML = `
         <form id="signUp-form">
@@ -114,32 +114,32 @@ getStartedButton.forEach((item) => {
             <input type="submit" id="signup-login" value="Sign up">
             </p>
         </form>`
-        
+
         // swiper-slide [0]
-    
+
         headerDiv[0].innerHTML = `<h1>Mange your day</h1>`
         messageDiv[0].innerHTML = `<p>From groceries to picking up the kids, we help you remember it all, anytime, anywhere.</p>`
         imageDiv[0].innerHTML = `<img src="./media/logIn-media/4703428.jpg">`
-    
+
         // swiper-slide [1]
-    
+
         headerDiv[1].innerHTML = `<h1>Get anything done</h1>`
         messageDiv[1].innerHTML = `<p>Create your perfect daily routine that works for you.</p>`
         imageDiv[1].innerHTML = `<img src="./media/logIn-media/4905798.jpg">`
-    
+
         // swiper-slide [2]
-    
+
         headerDiv[2].innerHTML = `<h1>Never forget a thing</h1>`
         messageDiv[2].innerHTML = `<p>Easily add reminders for daily, weekly or monthly commitments.</p>`
         imageDiv[2].innerHTML = `<img src="./media/logIn-media/6334182.jpg">`
-    
+
         // swiper-slide [2]
-    
+
         headerDiv[3].innerHTML = `<h1>Live Strategically</h1>`
         messageDiv[3].innerHTML = `<p>Focus on the things that matter to you unlock your true potential</p>`
         imageDiv[3].innerHTML = `<img src="./media/logIn-media/5243332.jpg">`
-        
-       
+
+
 
         const logInSwiper = new Swiper('.swiper-log-in', {
             // Optional parameters
@@ -160,14 +160,14 @@ getStartedButton.forEach((item) => {
                 delay: 7000,
             },
             effect: 'fade',
-            
+
         });
 
-    
+
         const SignUpSwiper = new Swiper('.right-swiper', {
             // Optional parameters
             direction: 'horizontal',
-            loop: true,
+            loop: false,
             keyboardControl: false,
             grabCursor: false,
             centeredSlides: true,
@@ -176,10 +176,10 @@ getStartedButton.forEach((item) => {
             allowTouchMove: false,
             speed: 800,
             effect: 'flip',
-            
+
         });
-        
-        document.onkeydown = function(evt) {
+
+        document.onkeydown = function (evt) {
             evt = evt || window.event;
             if (evt.key == "Escape") {
                 mainPage.classList.remove("hidden");
@@ -191,22 +191,16 @@ getStartedButton.forEach((item) => {
                 imageDiv = [];
             }
         };
-        (function($) { "use strict";
+        (function ($) {
+            "use strict";
             // navigation
-            $('a[data-slide]').click(function(e) {
-            e.preventDefault();
-            SignUpSwiper.slideTo($(this).data('slide'));
-        });
+            $('a[data-slide]').click(function (e) {
+                e.preventDefault();
+                SignUpSwiper.slideTo($(this).data('slide'));
+            });
         })(jQuery);
-        
+
     })
-    
-    
+
+
 })
-
-
-
-
-
-
-
--- src/script.js
+++ src/script.js
@@ -1,112 +1,150 @@
 // Swiper
 
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    keyboardControl: true,
-    grabCursor: false,
-    centeredSlides: true,
-    slidesPerView: 'auto',
-    initialSlide: 2,
-    allowTouchMove: false,
-    speed: 600,
+  // Optional parameters
+  direction: 'horizontal',
+  keyboardControl: true,
+  grabCursor: false,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  initialSlide: 2,
+  allowTouchMove: false,
+  speed: 600,
 });
 
 // Scroll page on refresh
 
-window.onbeforeunload = () => {  
-    window.scrollTo(0, 0);  
+window.onbeforeunload = () => {
+  window.scrollTo(0, 0);
 };
 
-$(document).ready(function(){
-    $(window).scroll(function(){
-        $(".icon-scroll").css("opacity", 1 - $(window).scrollTop() / $('.icon-scroll').height());
-    });
+$(document).ready(function () {
+  $(window).scroll(function () {
+    $(".icon-scroll").css("opacity", 1 - $(window).scrollTop() / $('.icon-scroll').height());
   });
-
-(function($) { "use strict";
-// navigation
-$('a[data-slide]').click(function(e) {
-  e.preventDefault();
-  swiper.slideTo($(this).data('slide'));
 });
+
+(function ($) {
+  "use strict";
+  // navigation
+  $('a[data-slide]').click(function (e) {
+    e.preventDefault();
+    swiper.slideTo($(this).data('slide'));
+  });
 })(jQuery);
 
-window.addEventListener('load',function(){
-    document.getElementsByTagName('header')[0].addEventListener('click',function(){
-      toggle_menu();
+window.addEventListener('load', function () {
+  document.getElementsByTagName('header')[0].addEventListener('click', function () {
+    toggle_menu();
+  });
+});
+
+window.addEventListener('scroll', function () {
+  header_scroll();
+
+});
+
+function header_scroll() {
+  var my_header = document.getElementsByTagName('header')[0];
+  var scroll_height = window.pageYOffset;
+  if (scroll_height > 0) {
+    my_header.className = 'header header-scroll';
+
+  }
+  else {
+    my_header.className = 'header';
+  }
+}
+
+function toggle_menu() {
+  var menu_bar = document.querySelector('.header ul');
+  var btn = document.getElementsByClassName('toggle-btn')[0];
+  //btn.innerHTML = '×';
+  if (document.documentElement.clientWidth <= 768) {
+    //if(menu_bar.style.display == 'none'){
+    if (menu_bar.className == 'display-none-mobile') {
+      menu_bar.className = 'display-block';
+      menu_bar.style.animation = 'flip 1 0.4s 0s';
+      btn.innerHTML = '×';
+      btn.style.fontSize = '40px';
+      btn.style.animation = 'fade 1 0.2s 0s';
+    }
+    else if (menu_bar.className == 'display-block') {
+      menu_bar.className = 'display-none-mobile';
+      btn.innerHTML = '☰';
+      btn.style.fontSize = '30px';
+      btn.style.animation = 'top-in-basic 1 0.2s 0s';
+    }
+  }
+}
+
+const nav = () => {
+  const nav = document.querySelector(".js-nav");
+  const navLinks = nav.querySelectorAll(".nav__link");
+  const slideRect = nav.querySelector(".nav__slider-rect");
+
+  nav.addEventListener("click", (evt) => {
+    if (!evt.target.classList.contains("nav__link")) {
+      return;
+    }
+    evt.preventDefault();
+
+    navLinks.forEach((item) => {
+      item.classList.remove("nav__link_active");
+    });
+
+    if (!evt.target.classList.contains("nav__link_active")) {
+      evt.target.classList.add("nav__link_active");
+    }
+
+    slideRect.style.transform = `translateX(${evt.target.dataset.transform}%)`;
+  });
+};
+nav();
+
+$(function () {
+  var $blocks = $('.animBlock.notViewed');
+  var $window = $(window);
+
+  $window.on('scroll', function (e) {
+    $blocks.each(function (i, elem) {
+      if ($(this).hasClass('viewed'))
+        return;
+
+      isScrolledIntoView($(this));
     });
   });
-
-window.addEventListener('scroll',function(){
- header_scroll();
-    
 });
-  
-function header_scroll(){
-    var my_header = document.getElementsByTagName('header')[0];
-    var scroll_height = window.pageYOffset;
-    if(scroll_height > 0){
-      my_header.className = 'header header-scroll';
-      
-    }
-    else {
-      my_header.className = 'header';
+
+function isScrolledIntoView(elem) {
+  var docViewTop = $(window).scrollTop();
+  var docViewBottom = docViewTop + $(window).height();
+  var elemOffset = 0;
+
+  if (elem.data('offset') != undefined) {
+    elemOffset = elem.data('offset');
+  }
+  var elemTop = $(elem).offset().top;
+  var elemBottom = elemTop + $(elem).height();
+
+  if (elemOffset != 0) { // custom offset is updated based on scrolling direction
+    if (docViewTop - elemTop >= 0) {
+      // scrolling up from bottom
+      elemTop = $(elem).offset().top + elemOffset;
+    } else {
+      // scrolling down from top
+      elemBottom = elemTop + $(elem).height() - elemOffset
     }
   }
 
-function toggle_menu(){
-    var menu_bar = document.querySelector('.header ul');
-    var btn = document.getElementsByClassName('toggle-btn')[0];
-    //btn.innerHTML = '&times;';
-    if(document.documentElement.clientWidth <= 768){
-    //if(menu_bar.style.display == 'none'){
-      if(menu_bar.className == 'display-none-mobile'){
-        menu_bar.className = 'display-block';
-        menu_bar.style.animation = 'flip 1 0.4s 0s';
-        btn.innerHTML = '&times;';
-        btn.style.fontSize = '40px';
-        btn.style.animation = 'fade 1 0.2s 0s';
-      }
-      else if(menu_bar.className == 'display-block'){
-       menu_bar.className = 'display-none-mobile';
-        btn.innerHTML = '&#9776';
-        btn.style.fontSize = '30px';
-        btn.style.animation = 'top-in-basic 1 0.2s 0s';
-      }
-   }
+  if ((elemBottom <= docViewBottom) && (elemTop >= docViewTop)) {
+    // once an element is visible exchange the classes
+    $(elem).removeClass('notViewed').addClass('viewed');
+
+    var animElemsLeft = $('.animBlock.notViewed').length;
+    if (animElemsLeft == 0) {
+      // with no animated elements left debind the scroll event
+      $(window).off('scroll');
+    }
   }
-
-  const nav = () => {
-    const nav = document.querySelector(".js-nav");
-    const navLinks = nav.querySelectorAll(".nav__link");
-    const slideRect = nav.querySelector(".nav__slider-rect");
- 
-    nav.addEventListener("click", (evt) => {
-       if (!evt.target.classList.contains("nav__link")) {
-          return;
-       }
-       evt.preventDefault();
- 
-       navLinks.forEach((item) => {
-          item.classList.remove("nav__link_active");
-       });
- 
-       if (!evt.target.classList.contains("nav__link_active")) {
-          evt.target.classList.add("nav__link_active");
-       }
- 
-       slideRect.style.transform = `translateX(${evt.target.dataset.transform}%)`;
-    });
- };
- nav();
-
-
- 
- 
-
-
-
-
-
-
+}
